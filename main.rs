cfe019c8e412b0524849599f81a16a3ca572f3b8
Phillip Adair Stewart Whelan <pwhelan@exis.cl>
2025-10-19 18:13:56 -0300

    initial working version.
    this is a working version of the server that can handle a single client
    at a time.
    
    Signed-off-by: Phillip Adair Stewart Whelan <pwhelan@exis.cl>

--- main.rs
+++ main.rs
@@ -1,50 +1,110 @@
-#[allow(unused_imports)]
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read,Write};
+use std::collections::HashMap;
 
-use std::mem::MaybeUninit;
-use std::net::{SocketAddr, TcpListener};
-use socket2::{Socket, Domain, Type};
+fn split_command(buf: &str) -> Vec<&str> {
+	buf.split_whitespace().clone().collect()
+}
 
-fn main() {
-	let srvsocket: Socket;
+enum HandleConnectionError {
+	UTF8Error(std::string::FromUtf8Error),
+	IOError(std::io::Error),
+}
 
-	let srvsocketres = Socket::new(Domain::IPV4, Type::STREAM, None);
-	match srvsocketres {
-		Ok(socket) => {
-			srvsocket = socket
+impl std::fmt::Display for HandleConnectionError {
+	fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			HandleConnectionError::IOError(err) => {
+				fmt.write_str(&err.to_string())
+			},
+			HandleConnectionError::UTF8Error(err) => {
+				fmt.write_str(&err.to_string())			
+			}
+		}
+	}
+}
+
+fn handle_connection(cache: &mut HashMap<String, String>, mut stream: TcpStream) -> Result<usize, HandleConnectionError> {
+	let mut buf: Vec<u8> = vec![0; 1024];
+	match stream.read(&mut buf) {
+		Ok(_) => {},
+		Err(err) => {
+			return Err(HandleConnectionError::IOError(err))
+		}
+	};
+
+	match String::from_utf8(buf) {
+		Ok(commands) => {
+			let command_tokens = split_command(&commands);
+
+			if command_tokens[0] == "GET" {
+				match cache.get(command_tokens[1]) {
+					Some(value) => {
+						let response = format!("FOUND:{}", value);
+						match stream.write(response.as_bytes()) {
+							Ok(_) => {},
+							Err(err) => {
+								return Err(HandleConnectionError::IOError(err));
+							}
+						};
+					},
+					None => {
+						match stream.write("NOT_FOUND".as_bytes()) {
+							Ok(_) => {},
+							Err(err) => {
+								return Err(HandleConnectionError::IOError(err));
+							}
+						};
+					},
+				};
+			} else if command_tokens[0] == "SET" {
+				let key = command_tokens[1].to_owned();
+				let val = command_tokens[2].to_owned();
+
+				cache.insert(key, val);
+
+				match stream.write("OK".as_bytes()) {
+					Ok(_) => {},
+					Err(err) => {
+						return Err(HandleConnectionError::IOError(err));
+					}
+				};
+			} else {
+				match stream.write("ERR:BAD_COMMAND".as_bytes()) {
+					Ok(_) => {},
+					Err(err) => {
+						return Err(HandleConnectionError::IOError(err));
+					}
+				};
+			}
 		},
-		Err(error) => {
-			panic!("Problem opening the socket: {}!", error);
+		Err(err) => {
+			return Err(HandleConnectionError::UTF8Error(err))
+		},
+	};
+
+	Ok(1)
+}
+
+fn main() -> std::io::Result<()> {
+	let listener = TcpListener::bind("127.0.0.1:35545")?;
+	let mut cache: HashMap<String, String> = HashMap::new();
+
+
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				match handle_connection(&mut cache, stream) {
+					Ok(_) => {},
+					Err(err) => {
+						panic!("Error: {}", err)
+					}
+				}
+			}
+			Err(err) => {
+				return Err(err)
+			}
 		}
 	}
-
-	let address: SocketAddr = "0.0.0.0:35545".parse().unwrap();
-	let address = address.into();
-	let res = srvsocket.bind(&address);
-	match res {
-		Ok(_) => {},
-		Err(error) => {
-			panic!("unable to listen: {}!", error)
-		}
-	}
-
-	let res = srvsocket.listen(5);
-	match res {
-		Ok(_) => {},
-		Err(error) => {
-			panic!("unable to listen: {}!", error)
-		}
-	}
-
-	loop {
-		let accepted = srvsocket.accept();
-		match accepted {
-			Ok((socket, _)) => {
-				let mut buf: MaybeUninit<u8> = MaybeUninit::uninit();
-				socket.recv(buf.as_bytes_mut());
-			},
-			Err(_) => {
-
-			},
-		}
-	}
+	Ok(())
 }
