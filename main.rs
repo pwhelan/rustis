6d68c5d21aebd3674950693df83374a751302570
Phillip Adair Stewart Whelan <pwhelan@exis.cl>
2025-10-19 22:24:21 -0300

    move server protocol code into a loop.
    move the server protocol code into a loop so it can continuously
    service a single client.
    
    Signed-off-by: Phillip Adair Stewart Whelan <pwhelan@exis.cl>

--- main.rs
+++ main.rs
@@ -7,40 +7,44 @@ fn split_command(buf: &str) -> Vec<&str> {
 }
 
 fn handle_connection(cache: &mut HashMap<String, String>, mut stream: TcpStream) -> Result<usize, Box<dyn std::error::Error>> {
-	let mut buf: Vec<u8> = vec![0; 1024];
-	stream.read(&mut buf)?;
 
-	match String::from_utf8(buf) {
-		Ok(commands) => {
-			let command_tokens = split_command(&commands);
+	loop {
+		let mut buf: Vec<u8> = vec![0; 1024];
+		stream.read(&mut buf)?;
 
-			if command_tokens[0] == "GET" {
-				match cache.get(command_tokens[1]) {
-					Some(value) => {
-						let response = format!("OK:{}", value);
-						stream.write(response.as_bytes())?;
-					},
-					None => {
-						stream.write("ERR:NOT_FOUND".as_bytes())?;
-					},
-				};
-			} else if command_tokens[0] == "SET" {
-				let key = command_tokens[1].to_owned();
-				let val = command_tokens[2].to_owned();
+		match String::from_utf8(buf) {
+			Ok(commands) => {
+				let command_tokens = split_command(&commands);
 
-				cache.insert(key, val);
+				if command_tokens[0] == "GET" {
+					match cache.get(command_tokens[1]) {
+						Some(value) => {
+							let response = format!("OK:{}", value);
+							stream.write(response.as_bytes())?;
+						},
+						None => {
+							stream.write("ERR:NOT_FOUND".as_bytes())?;
+						},
+					};
+				} else if command_tokens[0] == "SET" {
+					let key = command_tokens[1].to_owned();
+					let val = command_tokens[2].to_owned();
 
-				stream.write("OK".as_bytes())?;
-			} else {
-				stream.write("ERR:BAD_COMMAND".as_bytes())?;
-			}
-		},
-		Err(err) => {
-			return Err(Box::new(err))
-		},
-	};
+					cache.insert(key, val);
 
-	Ok(1)
+					stream.write("OK".as_bytes())?;
+				} else if command_tokens[0] == "QUIT" {
+					return Ok(1);
+				} else {
+					stream.write("ERR:BAD_COMMAND".as_bytes())?;
+				}
+			},
+			Err(err) => {
+				return Err(Box::new(err))
+			},
+		};
+	}
+
 }
 
 fn main() -> std::io::Result<()> {
