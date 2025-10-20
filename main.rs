bb79cbe449793d8df5ba6fe62b025287d059da6c
Phillip Adair Stewart Whelan <pwhelan@exis.cl>
2025-10-19 22:39:25 -0300

    move the cache into RwLock.
    move the HashMap cache into a RwLock in a first attempt to move towards
    servicing multiple clients at once.
    
    Signed-off-by: Phillip Adair Stewart Whelan <pwhelan@exis.cl>

--- main.rs
+++ main.rs
@@ -1,12 +1,13 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read,Write};
 use std::collections::HashMap;
+use std::sync::RwLock;
 
 fn split_command(buf: &str) -> Vec<&str> {
 	buf.split_whitespace().clone().collect()
 }
 
-fn handle_connection(cache: &mut HashMap<String, String>, mut stream: TcpStream) -> Result<usize, Box<dyn std::error::Error>> {
+fn handle_connection(cache_lock: &RwLock<HashMap<String, String>>, mut stream: TcpStream) -> Result<usize, Box<dyn std::error::Error>> {
 
 	loop {
 		let mut buf: Vec<u8> = vec![0; 1024];
@@ -17,22 +18,36 @@ fn handle_connection(cache: &mut HashMap<String, String>, mut stream: TcpStream)
 				let command_tokens = split_command(&commands);
 
 				if command_tokens[0] == "GET" {
-					match cache.get(command_tokens[1]) {
-						Some(value) => {
-							let response = format!("OK:{}", value);
-							stream.write(response.as_bytes())?;
+					match cache_lock.read() {
+						Ok(cache) => {
+							match cache.get(command_tokens[1]) {
+								Some(value) => {
+									let response = format!("OK:{}", value);
+									stream.write(response.as_bytes())?;
+								},
+								None => {
+									stream.write("ERR:NOT_FOUND".as_bytes())?;
+								},
+							};
 						},
-						None => {
-							stream.write("ERR:NOT_FOUND".as_bytes())?;
-						},
-					};
+						Err(_) => {
+							panic!("CacheLock has been poisoned");
+						}
+					}
 				} else if command_tokens[0] == "SET" {
-					let key = command_tokens[1].to_owned();
-					let val = command_tokens[2].to_owned();
+					match cache_lock.write() {
+						Ok(mut cache) => {
+							let key = command_tokens[1].to_owned();
+							let val = command_tokens[2].to_owned();
 
-					cache.insert(key, val);
+							cache.insert(key, val);
 
-					stream.write("OK".as_bytes())?;
+							stream.write("OK".as_bytes())?;
+						},
+						Err(_) => {
+							panic!("CacheLock has been poisoned");
+						}
+					}
 				} else if command_tokens[0] == "QUIT" {
 					return Ok(1);
 				} else {
@@ -49,15 +64,16 @@ fn handle_connection(cache: &mut HashMap<String, String>, mut stream: TcpStream)
 
 fn main() -> std::io::Result<()> {
 	let listener = TcpListener::bind("127.0.0.1:35545")?;
-	let mut cache: HashMap<String, String> = HashMap::new();
+	let cache: HashMap<String, String> = HashMap::new();
+	let cache_lock = RwLock::new(cache);
 
 	for stream in listener.incoming() {
 		match stream {
 			Ok(stream) => {
-				match handle_connection(&mut cache, stream) {
+				match handle_connection(&cache_lock, stream) {
 					Ok(_) => {},
 					Err(err) => {
-						panic!("Error: {}", err)
+						panic!("Stream Error: {}", err)
 					}
 				}
 			}
