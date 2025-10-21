5230424a2083ad8e0f1e75752b01c30823cfd2ad
Phillip Adair Stewart Whelan <pwhelan@exis.cl>
2025-10-20 21:53:49 -0300

    use function pointers and Command struct for declaring commands.
    Signed-off-by: Phillip Adair Stewart Whelan <pwhelan@exis.cl>

--- dispatch.rs
+++ dispatch.rs
@@ -3,51 +3,126 @@ use std::io::Write;
 use std::collections::HashMap;
 use std::sync::RwLock;
 
-pub fn dispatch_command(cache_lock: &RwLock<HashMap<String, String>>, stream: &mut TcpStream, command_tokens: Vec<&str>) -> Result<usize, Box<dyn std::error::Error>> {
-	if command_tokens[0] == "GET" {
-		if command_tokens.len() < 2 {
-			stream.write("ERR:MISSING_ARGUMENTS\n".as_bytes())?;
-			return Ok(1)
-		}
-		match cache_lock.read() {
-			Ok(cache) => {
-				match cache.get(command_tokens[1]) {
-					Some(value) => {
-						let response = format!("OK:{}\n", value);
-						stream.write(response.as_bytes())?;
-					},
-					None => {
-						stream.write("ERR:NOT_FOUND\n".as_bytes())?;
-					},
-				};
-			},
-			Err(_) => {
-				panic!("CacheLock has been poisoned");
-			}
-		}
-	} else if command_tokens[0] == "SET" {
-		if command_tokens.len() < 2 {
-			stream.write("ERR:MISSING_ARGUMENTS\n".as_bytes())?;
-			return Ok(1)
-		} 
-		match cache_lock.write() {
-			Ok(mut cache) => {
-				let key = command_tokens[1].to_owned();
-				let val = command_tokens[2].to_owned();
+#[derive(PartialEq)]
+pub enum Status {
+	Error,
+	Ok,
+	Quit,
+}
 
-				cache.insert(key, val);
+struct Response {
+	status: Status,
+	payload: String,
+}
 
-				stream.write("OK\n".as_bytes())?;
-			},
-			Err(_) => {
-				panic!("CacheLock has been poisoned");
-			}
+struct Command {
+	verb: String,
+	arguments: usize,
+	function: fn(&RwLock<HashMap<String, String>>, command_tokens: Vec<&str>)
+		-> Result<Response, Box<dyn std::error::Error>>,
+}
+
+fn execute_get(
+	cache_lock: &RwLock<HashMap<String, String>>,
+	command_tokens: Vec<&str>
+) -> Result<Response, Box<dyn std::error::Error>>
+{
+	match cache_lock.read() {
+		Ok(cache) => {
+			match cache.get(command_tokens[1]) {
+				Some(value) => {
+					return Ok(Response{
+						status: Status::Ok,
+						payload: String::from(format!("OK:{}\n", value)),
+					});
+				},
+				None => {
+					return Ok(Response{
+						status: Status::Error,
+						payload: String::from("ERR:NOT_FOUND\n"),
+					});
+				},
+			};
+		},
+		Err(_) => {
+			panic!("CacheLock has been poisoned");
 		}
-		
-	} else if command_tokens[0] == "QUIT" {
-		return Ok(1);
-	} else {
-		stream.write("ERR:BAD_COMMAND\n".as_bytes())?;
 	}
-	Ok(1)
+}
+
+fn execute_set(
+	cache_lock: &RwLock<HashMap<String, String>>,
+	command_tokens: Vec<&str>
+)  -> Result<Response, Box<dyn std::error::Error>>
+{
+	match cache_lock.write() {
+		Ok(mut cache) => {
+			let key = command_tokens[1].to_owned();
+			let val = command_tokens[2].to_owned();
+
+			cache.insert(key, val);
+			return Ok(Response{
+				status: Status::Ok,
+				payload: String::from("OK\n"),
+			});
+		},
+		Err(_) => {
+			panic!("CacheLock has been poisoned");
+		}
+	}
+}
+
+fn execute_quit(
+	_cache_lock: &RwLock<HashMap<String, String>>,
+	_command_tokens: Vec<&str>
+) -> Result<Response, Box<dyn std::error::Error>>
+{
+	return Ok(Response{
+		status: Status::Quit,
+		payload: String::from("OK\n"),
+	})
+}
+
+pub fn dispatch_command(
+	cache_lock: &RwLock<HashMap<String, String>>,
+	stream: &mut TcpStream,
+	command_tokens: Vec<&str>
+) -> Result<Status, Box<dyn std::error::Error>> 
+{
+	let commands = vec![
+		Command { 
+			verb: String::from("GET"),
+			arguments: 1,
+			function: execute_get,
+		},
+		Command {
+			verb: String::from("SET"),
+			arguments: 2,
+			function: execute_set,
+		},
+		Command {
+			verb: String::from("QUIT"),
+			arguments: 0,
+			function: execute_quit,
+		}
+	];
+
+	for i in 0..commands.len() -1 {
+		if command_tokens[0] == commands[i].verb {
+			if command_tokens.len() < commands[i].arguments {
+				println!("THIS COMMAND IS MISSING ARGUMENTS: {} < {}", 
+					command_tokens.len(), commands[i].arguments);
+				stream.write("ERR:MISSING_ARGUMENTS\n".as_bytes())?;
+				return Ok(Status::Error)
+			}
+
+			let resp = (commands[i].function)(cache_lock, command_tokens)?;
+			stream.write(resp.payload.as_bytes())?;
+
+			return Ok(resp.status)
+		}
+	}
+
+	stream.write("ERR:BAD_COMMAND\n".as_bytes())?;
+	return Ok(Status::Error);
 }
--- main.rs
+++ main.rs
@@ -10,8 +10,11 @@ fn split_command(buf: &str) -> Vec<&str> {
 	buf.split_whitespace().clone().collect()
 }
 
-fn handle_connection(cache_lock: &RwLock<HashMap<String, String>>, mut stream: TcpStream) -> Result<usize, Box<dyn std::error::Error>> {
-
+fn handle_connection(
+	cache_lock: &RwLock<HashMap<String, String>>,
+	mut stream: TcpStream
+) -> Result<dispatch::Status, Box<dyn std::error::Error>>
+{
 	loop {
 		let mut buf: Vec<u8> = vec![0; 1024];
 		stream.read(&mut buf)?;
@@ -19,7 +22,12 @@ fn handle_connection(cache_lock: &RwLock<HashMap<String, String>>, mut stream: T
 		match String::from_utf8(buf) {
 			Ok(commands) => {
 				let command_tokens = split_command(&commands);
-				dispatch::dispatch_command(cache_lock, &mut stream, command_tokens)?;
+				let response = dispatch::dispatch_command(cache_lock, &mut stream,
+					command_tokens)?;
+
+				if response == dispatch::Status::Quit {
+					return Ok(response)
+				}
 			},
 			Err(err) => {
 				return Err(Box::new(err))
